1354f782b96131c2d6a106271e7978bd595d6887
ludesmo <lu.paredesm@duocuc.cl>
2025-04-08 13:11:49 -0400

    parte de login

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  step = 1;
 
+  nombre = '';
+  apellido = '';
+  fechaNacimiento = '';
+  contrasena = '';
+
+  irAlSiguientePaso() {
+    this.step++;
+  }
 }

b4386f03dd41ea4063f1379df544b3aa4c76421f
MartRomeros <mart.romeros@duocuc.cl>
2025-04-14 11:47:54 -0400

    agregando validaciones al login y sweetalert2 usar npm i

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,24 +1,49 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { signal } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2'
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule, MatFormFieldModule, MatInputModule,MatIconModule,MatButtonModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, ReactiveFormsModule, SweetAlert2Module],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 
 export class LoginComponent {
 
+  //creacion de la contrasenna
+  loginForm!: FormGroup
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    })
+  }
+
+  //visibilidad para la contrasenna
   hide = signal(true);
   clickEvent(event: MouseEvent) {
     this.hide.set(!this.hide());
     event.stopPropagation();
+  }
+
+  //validar campos y mostrarlo en pantalla
+  hasErrors(campo: string, typeError: string) {
+    return this.loginForm.get(campo)?.touched && this.loginForm.get(campo)?.hasError(typeError)
+  }
+
+  login() {
+    //validar campos
+    Swal.fire("SweetAlert2 is working!");
+
   }
 
 

779d77c1e3758b8c1da95a64fbf2b74760e108f6
MartRomeros <mart.romeros@duocuc.cl>
2025-04-14 09:31:39 -0400

    agregando angular material

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -2,15 +2,24 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { signal } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule,MatFormFieldModule,MatInputModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule,MatIconModule,MatButtonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'  
+  styleUrl: './login.component.css'
 })
 
 export class LoginComponent {
-  
+
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+
 
 }

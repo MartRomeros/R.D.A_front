34c67b3a0a4344827a5caef51fcaa1dff421fd29
MartRomeros <mart.romeros@duocuc.cl>
2025-04-28 09:04:01 -0400

    enrutando reportes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { LoginComponent } from './components/auth/login/login.component';
 import { RegistroComponent } from './components/auth/registro/registro.component';
 import { ForgotPasswordComponent } from './components/auth/forgot-password/forgot-password.component';
 import { AlumnoComponent } from './components/alumno/alumno.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { ReportesComponent } from './components/admin/reportes/reportes.component';
 
 export const routes: Routes = [
     {path:'',component:LoginComponent},
@@ -10,8 +12,8 @@ export const routes: Routes = [
     //ruta login component
     {path:'login',component:LoginComponent},
     {path:'alumno',component:AlumnoComponent},
-    //ruta registro componente
-    {path:'registro',component:RegistroComponent},
+    {path:'admin',component:AdminComponent},
+    {path:'reportes',component:ReportesComponent},
 
     {path:'forgot-password',component:ForgotPasswordComponent},
 
